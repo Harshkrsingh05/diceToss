ae9e5ea2550807c936e7a641e1d760f208f163fa
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2022-12-30 20:50:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let randomNumber1= Math.floor(Math.random() *6) +1;
+
+let randomDiceImage = "dice"+randomNumber1+".png";
+
+let randomImageSource = "images/"+ randomDiceImage;
+
+let image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+
+let randomNumber2=  Math.floor(Math.random()*6)+1;
+let randomImageSource2= "images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML=("🚩Player 1 wins!");
+}
+else if(randomNumber2>randomNumber1){
+  document.querySelector("h1").innerHTML=("Player 2 wins!🚩");
+}
+else{
+  document.querySelector("h1").innerHTML=("Draw!🏳");
+}
